cc4f54471095c3348fb1bd28164a76e3a309a290
Thomas Liebeskind <thomas.liebeskind@audi.de>
2019-07-03 23:01:35 +0200

    Update the example

--- example/main.go
+++ example/main.go
@@ -9,5 +9,8 @@ import (
 )
 
 func main() {
-	fmt.Println(config.Development.Version)
+	fmt.Println(config.Current.Version)
+	fmt.Println(config.Current.Randomizer.Threshold)
+	fmt.Println(config.Current.Secrets)
+	fmt.Println(config.Current.Server.Port)
 }
