4c0146c401a5a275ad21af78539015d2cae6587a
Thomas Liebeskind <thomas.liebeskind@gmail.com>
2019-07-09 23:03:57 +0200

    Refactor dotenv parser to allow comments, check key for validity, allow : as key-value separator, allow - as Env separator, add comments

--- parsers/dotenv.go
+++ parsers/dotenv.go
@@ -5,13 +5,26 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"regexp"
 	"strings"
 
 	mergo "github.com/imdario/mergo"
 	"github.com/theliebeskind/genfig/util"
 )
 
-// DotenvStrategy parses yaml and json files
+var (
+	allowedKVSeparators  = []string{"=", ":"}
+	allowedEnvSeparators = []string{"_", "-"}
+	keyRegex             = regexp.MustCompile(`^[A-Za-z][A-Za-z0-9]*$`)
+)
+
+// DotenvStrategy parses .env files.
+// Emply lines are ignored.
+// Allowed key-value separators are only "=" and ":".
+// Supported values are: string, int64, float64, bool and json arrays,
+// e.g. `["a", "b", "c"]` ([]string) or `["a", 1, true]` ([]interface {}).
+// Key can be nests by eithe one of the allowed env separators, e.g. `DB_NAME` or `SERVER-HOST`
+// Comments are only allowed in seperate lines.
 type DotenvStrategy struct {
 }
 
@@ -26,22 +39,38 @@ func (s *DotenvStrategy) Parse(data []byte) (map[string]interface{}, error) {
 	scanner := bufio.NewScanner(bytes.NewBuffer(data))
 
 	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.TrimSpace(line) == "" {
+		line := strings.TrimSpace(scanner.Text())
+		// ignore empty or comment lines
+		if line == "" || strings.HasPrefix(line, "#") {
 			continue
 		}
+		// try to split line into key and value by allowed separators
 		var kv []string
-		if kv = strings.SplitN(line, "=", 2); len(kv) != 2 {
-			if kv = strings.SplitN(line, ":", 2); len(kv) != 2 {
+		for i, sep := range allowedKVSeparators {
+			if kv = strings.SplitN(line, sep, 2); len(kv) == 2 {
+				break
+			}
+			if i == len(allowedKVSeparators)-1 {
 				return nil, fmt.Errorf("Invalid dotenv line: '%s'", line)
 			}
 		}
+
+		// trim key and value
 		k := strings.TrimSpace(kv[0])
 		v := strings.TrimSpace(kv[1])
-		keys := strings.Split(strings.ToLower(k), "_")
+
+		var keys []string
+		for _, sep := range allowedEnvSeparators {
+			if keys = strings.Split(strings.ToLower(k), sep); len(keys) > 1 {
+				break
+			}
+		}
 
 		var item interface{} = r
 		for i, key := range keys {
+			if !keyRegex.MatchString(key) {
+				return nil, fmt.Errorf("Key '%s' is not valid", key)
+			}
 			if item, found := item.(map[string]interface{})[key]; found {
 				switch item.(type) {
 				case map[string]interface{}:
