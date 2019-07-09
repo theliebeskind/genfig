659c2a152134ab9671e78b5cdc5a81b1e2a8e630
Thomas Liebeskind <thomas.liebeskind@gmail.com>
2019-07-09 23:42:28 +0200

    Add plugin to generate test

---
+++ plugins/tester.go
@@ -0,0 +1,48 @@
+package plugins
+
+import (
+	"io"
+	"text/template"
+
+	"github.com/theliebeskind/genfig/types"
+)
+
+type configTestPlugin struct {
+	tpl *template.Template
+}
+
+var (
+	configTest = configTestPlugin{
+		tpl: template.Must(template.
+			New("configTest").
+			Parse(`import "testing"
+
+func Test_CurrentConfig(t *testing.T) {
+	if Current == nil {
+		t.Error("Current config is nil")
+	}
+}
+`))}
+)
+
+func init() {
+	// "register" plugin
+	Plugins["config_test"] = &configTest
+}
+
+// GetInitCall returns the availibility and the string of the
+// function to be called on init
+func (p *configTestPlugin) GetInitCall() (string, bool) {
+	return "", false
+}
+
+// SetSchemaMap sets the schema to be used when WriteTo is called
+func (p *configTestPlugin) SetSchemaMap(_ types.SchemaMap) {
+}
+
+// WriteTo performs the acutal writing to a buffer (or io.Writer).
+// For this plugin, the template is simply "rendered" into the writer.
+func (p *configTestPlugin) WriteTo(w io.Writer) (l int64, err error) {
+	err = p.tpl.Execute(w, nil)
+	return
+}
